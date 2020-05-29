b045baed52325346f80ff7918ef82a943a84801e
Saadat-MBP16 <me@saadat.dev>
2020-05-29 12:00:41 +0200

    Add Header
    Signed-off-by: Saadat-MBP16 <me@saadat.dev>

---
+++ main.h
@@ -0,0 +1,17 @@
+//
+// Created by Saadat Baig on 28.05.20.
+//
+
+#ifndef DICEWARECPP_MAIN_H
+#define DICEWARECPP_MAIN_H
+
+#define error(msg) std::cout << "[-]: " << msg << std::endl;
+#define success(msg) std::cout << "[+]: " << msg << std::endl;
+#define info(msg) std::cout << "[*]: " << msg << std::endl;
+
+#include <iostream>
+#include <sstream>
+#include <random>
+#include "dicewareMap.h"
+
+#endif //DICEWARECPP_MAIN_H
