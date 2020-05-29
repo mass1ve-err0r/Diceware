0cfe1e97a4c1fd68c6dddd3022f0fadbe4224bd5
Saadat-MBP16 <me@saadat.dev>
2020-05-29 15:14:00 +0200

    Update Main - Add a fairly adequate RNG (see PEP506)
    Signed-off-by: Saadat-MBP16 <me@saadat.dev>

--- dwMain.py
+++ dwMain.py
@@ -1,5 +1,6 @@
 from dicewareMap import dicewareMap as dwMap
-from typing import Any
+from typing import List, Any
+from secrets import randbelow
 
 
 # <editor-fold desc="[Prettier Printing Functions]">
@@ -25,4 +26,22 @@ def info(msg: str, val: Any = None) -> None:
         return
     print("[*]: {}{}".format(msg, val))
     return
+# </editor-fold>
+
+
+# <editor-fold desc="[Internal Implementation Functions]">
+def getRandomNumber(exclusive_max: int, singular: bool) -> int:
+    if singular:
+        rv_singular = randbelow(exclusive_max)
+        return rv_singular
+    rv_s: str = ""
+    rNums: List[int] = []
+    for i in range(0, 5):
+        num: int = randbelow(exclusive_max)
+        if num == 0:
+            num = 1
+        rNums.append(num)
+    for i in rNums:
+        rv_s += str(i)
+    return int(rv_s)
 # </editor-fold>
