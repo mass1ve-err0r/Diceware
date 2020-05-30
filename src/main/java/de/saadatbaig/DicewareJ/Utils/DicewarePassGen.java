5a001926a5ba1fb12c0ad8f1f2adf19edf5d1673
Saadat-MBP16 <me@saadat.dev>
2020-05-30 13:28:59 +0200

    Update DicewarePassGen - Add proper init - Add lookup lists for exchangeable chars and special chars
    (!) Not all specials chars are used
    
    Signed-off-by: Saadat-MBP16 <me@saadat.dev>

--- src/main/java/de/saadatbaig/DicewareJ/Utils/DicewarePassGen.java
+++ src/main/java/de/saadatbaig/DicewareJ/Utils/DicewarePassGen.java
@@ -5,6 +5,14 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class DicewarePassGen {
 
+    private final char[] specials = new char[] {'<', '(', '{', '[', '\\', '^', '-', '=', '$', '!', '|', ']', '}', ')', '?', '+', '*', '.', '>', '_'};
+    private final char[] exchangeables = new char[] {'a', 'b', 'e', 'z', 's', 't', 'i', 'g', 'o'};
+    DicewareMap dwMap;
+
+    public DicewarePassGen() {
+        dwMap = new DicewareMap();
+    }
+
     public String generateRandomNumberString(int _min, int _max) {
         StringBuilder rv = new StringBuilder();
         for (int i = 0; i < 5; i++) {
