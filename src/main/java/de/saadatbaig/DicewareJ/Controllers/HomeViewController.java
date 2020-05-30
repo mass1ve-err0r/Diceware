f6d0d247bc3edf5b30bed8b20d78a2162049fc94
Saadat-MBP16 <me@saadat.dev>
2020-05-30 13:50:46 +0200

    Update HomeViewcontroller - Add logic to create passwords with details/ options provided --> passes the created password onto the View to show
    Signed-off-by: Saadat-MBP16 <me@saadat.dev>

--- src/main/java/de/saadatbaig/DicewareJ/Controllers/HomeViewController.java
+++ src/main/java/de/saadatbaig/DicewareJ/Controllers/HomeViewController.java
@@ -16,7 +16,23 @@ public class HomeViewController {
         dwManager = new DicewarePassGen();
     }
 
+    public void generatePassword(int _wc, boolean subWhitespaces, boolean addNums) {
+        String[] usedWords = dwManager.getWords(_wc);
+        String rv_s = dwManager.createSpacedString(usedWords);
 
+        if (subWhitespaces) {
+            rv_s = dwManager.substituteContentsWithSpecials(rv_s, 1);
+        } else {
+            rv_s = dwManager.substituteContentsWithSpecials(rv_s, 3);
+        }
+
+        if (addNums) {
+            rv_s = dwManager.substituteContentsWithSpecials(rv_s, 2);
+        }
+
+        Tuple<String[], String> rv = new Tuple<>(usedWords, rv_s);
+        hv.showPassword(rv);
+    }
 
     /* End */
 }
