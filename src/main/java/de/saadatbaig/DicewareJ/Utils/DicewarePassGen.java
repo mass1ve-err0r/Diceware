b9b9197d653602a9af737f9fabbb8ef2dc033213
Saadat-MBP16 <me@saadat.dev>
2020-05-30 13:27:39 +0200

    Update DicewarePassGen - Include ability to get a random number String --> Digits range from 1 to 6.
    Signed-off-by: Saadat-MBP16 <me@saadat.dev>

--- src/main/java/de/saadatbaig/DicewareJ/Utils/DicewarePassGen.java
+++ src/main/java/de/saadatbaig/DicewareJ/Utils/DicewarePassGen.java
@@ -5,6 +5,15 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class DicewarePassGen {
 
+    public String generateRandomNumberString(int _min, int _max) {
+        StringBuilder rv = new StringBuilder();
+        for (int i = 0; i < 5; i++) {
+            int rNum = generateSingularRandomNumber(_min, _max);
+            rv.append(rNum);
+        }
+        return rv.toString();
+    }
+
     public int generateSingularRandomNumber(int _min, int _max) {
         return ThreadLocalRandom.current().nextInt(_min, _max);
     }
